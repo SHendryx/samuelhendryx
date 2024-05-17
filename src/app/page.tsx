f18cda1290b1cb3127497f7eca6d3d9c1a2185ab
Samuel Hendryx <samuelhendryx@outlook.com>
2024-05-17 14:12:32 -0700

    Add name tag and photo to home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 // @/
+import Image from "next/image";
 import React from "react";
+import { RxWidth } from "react-icons/rx";
 
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-[#20487b] text-gray-200'>
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-xl'>
-        <div className='justify-center bg-[#1c325f] rounded-3xl p-4 border-4'>
-          <p>Hi, my name is</p>
-          <h1>Samuel Hendryx</h1>
+    <div className='text-gray-200 flex'>
+      <div className='min-h-[calc(100vh-85px)] max-w-[1000px] mx-auto px-2 text-xl flex flex-col justify-center items-center'>
+        <div className="flex space-x-10">
+        {/* name tag */}
+          <div className='h-[150px] w-[300px] text-center bg-[#1c325f] rounded-3xl border-4 -rotate-6'>
+            <div className='h-[35px] bg-[#1c325f] text-gray-200 rounded-3xl'><p>Hello, my name is</p></div>
+            <div className="h-[85px] bg-gray-200 text-black p-0 place-content-center"><h1>Samuel Hendryx</h1></div>
+            <div className="bg-[#1c325f]"></div>
+          </div>
+          <div>
+            <Image className='me_circle' src='/assets/images/photos/samuelhendryx.png' width='150' height='150' alt='Samuel Hendryx' />
+          </div>
+        </div>
+        {/* intro paragraph */}
+        <div className='p-4'>
           <p className='max-w-[700px]'>I am a software developer who graduated in Spring of 2020.</p>
-          <p className='py-2 max-w-[700px]'>I enjoy backend development in Python, and I&apos;m currently working on developing web applications in React.</p>
+          <p className='max-w-[700px]'>I enjoy backend development in Python, and I&apos;m currently working on developing web applications in React.</p>
         </div>
       </div>
     </div>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,16 +3,10 @@
 import React, {useState} from 'react';
 import Image from 'next/image';
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
-import logo from '@/assets/images/logos/sh_logo_256_256.png';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
-  const navStyle = {
-    backgroundImage: "url('/assets/images/backgrounds/sacramento-skyline-sunset.png')",
-    backgroundSize: "cover",
-    backgroundPositionY: "0%",
-    }
 
   return (
     <div className='nav fixed w-full h-[80px] flex justify-between items-center bg-[#20487b] text-gray-200'>
@@ -24,8 +18,8 @@ const Navbar = () => {
       <div>
         <ul className='hidden md:flex px-4 text-lg'>
           <li className='px-4'>Home</li>
-          <li className='px-4'>About</li>
           <li className='px-4'>Resume</li>
+          <li className='px-4'>About</li>
         </ul>
       </div>
 
@@ -47,8 +41,8 @@ const Navbar = () => {
       {/* mobile menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#20487b] flex flex-col justify-center items-center'}>
           <li className='py-6 text-4xl'>Home</li>
-          <li className='py-6 text-4xl'>About</li>
           <li className='py-6 text-4xl'>Resume</li>
+          <li className='py-6 text-4xl'>About</li>
         </ul>
     </div>
   )
